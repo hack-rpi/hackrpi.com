fdefb922636aa31c1d1eff66c47ba7f78778459d
Robert Rouhani <robert.rouhani@gmail.com>
2014-06-26 19:21:09 -0400

    Temporarily removed translation transitions

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -12,61 +12,71 @@ $(document).ready(function() {
 	var preregSuccess = document.getElementById("preregSuccess");
 	var preregSuccessWrap = document.getElementById("preregSuccessWrap");
 	
-	info.addEventListener("transitionend", function(event) {
+	/*info.addEventListener("transitionend", function(event) {
 		if (info !== event.target) return;
 		if (info.classList.contains("active"))
 			infoWrap.classList.add("active");
 		else
 			prereg.classList.add("active");
-	}, false);
+	}, false);*/
 	
 	infoWrap.addEventListener("transitionend", function(event) {
 		if (infoWrap !== event.target) return;
-		if (!infoWrap.classList.contains("active"))
+		if (!infoWrap.classList.contains("active")) {
 			info.classList.remove("active");
+			prereg.classList.add("active");
+			preregWrap.classList.add("active");
+		}
 	}, false);
 	
 	preregBtn.onclick = function() {
 		infoWrap.classList.remove("active");
 	}
 	
-	prereg.addEventListener("transitionend", function(event) {
+	/*prereg.addEventListener("transitionend", function(event) {
 		if (prereg !== event.target) return;
 		if (prereg.classList.contains("active"))
 			preregWrap.classList.add("active");
 		else
 			preregSuccess.classList.add("active");
 		
-	}, false);
+	}, false);*/
 	
 	preregWrap.addEventListener("transitionend", function(event) {
 		if (preregWrap !== event.target) return;
-		if (!preregWrap.classList.contains("active"))
+		if (!preregWrap.classList.contains("active")) {
 			prereg.classList.remove("active");
+			preregSuccess.classList.add("active");
+			preregSuccessWrap.classList.add("active");
+		}
 	}, false);
 	
-	preregSuccess.addEventListener("transitionend", function(event) {
+	/*preregSuccess.addEventListener("transitionend", function(event) {
 		if (preregSuccess !== event.target) return;
 		if (preregSuccess.classList.contains("active"))
 			preregSuccessWrap.classList.add("active");
 		else
 			info.classList.add("active");
 		
-	}, false);
+	}, false);*/
 	
 	preregSuccessWrap.addEventListener("transitionend", function(event) {
 		if (preregSuccessWrap !== event.target) return;
-		if (!preregSuccessWrap.classList.contains("active"))
+		if (!preregSuccessWrap.classList.contains("active")) {
 			preregSuccess.classList.remove("active");
+			info.classList.add("active");
+			infoWrap.classList.add("active");
+		}
 		else
 			setTimeout(function() {
 				preregSuccessWrap.classList.remove("active");
 			}, 3000);
 	}, false);
 	
-	setTimeout(function() {
+	/*setTimeout(function() {
 		info.classList.add("active");
-	}, 200);
+		infoWrap.classList.add("active");
+	}, 200);*/
 	
 	$("#preregForm").submit(function() {
 		$theForm = $(this);
