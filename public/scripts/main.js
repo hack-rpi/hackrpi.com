52575042ad637d6830129539abdc6f45663a243b
Ariel Lee <ariel.lee.1995@gmail.com>
2015-08-02 19:52:31 -0700

    Changed layout of 2015 site, put up basic information and preregistration form

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {	
 	document.body.setAttribute("class", "loaded");
+
+	$("#preregForm").submit(function() {
+		$theForm = $(this);
+		
+		$.ajax({
+			type: $theForm.attr("method"),
+			url: $theForm.attr("action"),
+			dataType: "xml",
+			data: $theForm.serialize(),
+		});
+		
+		// KNOWN ISSUE: Currently, the form will submit to Google Forms, 
+		// but there is no response back because of a cross domain issue
+		// that is out of our control. Since this is just preregistration,
+		// and needs to be up, I will leave this error, and hopefully we will
+		// use something better for actual registration.
+		$("#preregForm").hide();
+		$("#prereg-success").show();
+		return false;
+	});	
 });
